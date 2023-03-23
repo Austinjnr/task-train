789b6b1017007f90515d59d3a3d7c08ea1472507
Austinjnr <austinobonyo@gmail.com>
2023-03-23 17:08:17 +0300

    edited login form

--- src/components/form/signup.js
+++ src/components/form/signup.js
@@ -1,21 +1,25 @@
-function Login() {
+function Signup() {
     return ( 
         <section>
             <form>
-                <h3>Welcome back!</h3>
+                <h3>Create an Account</h3>
+                <div className="mb-3"> 
+                    <lable for="yourUserNameInput" className="form-lable">User Name</lable>
+                    <input type="name" className="form-control" id="yourUserNameInput" aria-describedby="usernameHelp" placeholder="Enter username" />
+                </div>
                 <div className="mb-3">
                     <lable for="yourEmailInput" className="form-lable">Email address</lable>
                     <input type="email" className="form-control" id="yourEmailInput" aria-describedby="emailHelp" placeholder="Enter email" />
                 </div>
                 <div className="mb-3 form-check">
-                <lable for="yourPasswordInput" className="form-lable">Password</lable>
+                 <lable for="yourPasswordInput" className="form-lable">Password</lable>
                     <input type="password" className="form-control" id="yourPasswordInput" aria-describedby="passwordHelp" placeholder="********" />
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
-                <button>Don't have an account, sign up here!</button>
+                <button>Already have an account, login here!</button>
             </form>
         </section>
      );
 }
 
-export default Login;
+export default Signup;
