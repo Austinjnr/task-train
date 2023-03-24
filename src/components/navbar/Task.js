e0f2844ff6fad929e819c2839da194a533042cf2
Austinjnr <austinobonyo@gmail.com>
2023-03-24 07:45:27 +0300

    adds task in list form

--- src/components/navbar/Task.js
+++ src/components/navbar/Task.js
@@ -29,6 +29,16 @@ function Task() {
         		/>
 				<button onClick={addTask}>+</button>
 			</div>
+			<ul>
+       			 {items.map((item) => (
+          			<Task
+            			key={items.indexOf(item)}
+            			id={items.indexOf(item)}
+            			value={item}
+          	  			onDelete={deleteTask}
+         			 />
+					 ))}
+      		</ul>
 		</div>
 	)
 }
