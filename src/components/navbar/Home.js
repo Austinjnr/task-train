b9511f1f2e3756318f74d83e2b8bdc7a66a22303
Austinjnr <austinobonyo@gmail.com>
2023-03-24 05:51:38 +0300

    adds home  component

---
+++ src/components/navbar/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return ( 
+        <section>
+            <div className="">
+                <h1>Home</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
