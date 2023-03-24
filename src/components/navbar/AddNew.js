a0d821e11fa489b8814fada71b545b10bda45dca
Austinjnr <austinobonyo@gmail.com>
2023-03-24 05:51:15 +0300

    adds edit task component

---
+++ src/components/navbar/AddNew.js
@@ -0,0 +1,11 @@
+function AddNew() {
+    return ( 
+        <section>
+            <div className="">
+                <h1>Skills</h1>
+            </div>
+        </section>
+     );
+}
+
+export default AddNew;
