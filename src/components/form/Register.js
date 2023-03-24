647acd0c2fd386c3a0bea2117780ced8a9fbeae4
Austinjnr <austinobonyo@gmail.com>
2023-03-24 05:34:10 +0300

    adds fuctionality to login page

--- src/components/form/Register.js
+++ src/components/form/Register.js
@@ -1,7 +1,19 @@
+import React, {useState} from 'react';
+import Login from './Login';
+import SignUp from './Signup';
+
 function Register() {
+    const [currentForm, setCurrentForm] = useState('signup');
+
+    const toggleForm = (formName) => {
+        setCurrentForm(formName);
+      }
+
     return ( 
         <div>
-            
+            {
+        currentForm === "signup" ? <SignUp onFormSwitch={toggleForm} /> : <Login onFormSwitch={toggleForm} />
+      }
         </div>
      );
 }
