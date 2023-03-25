14c9871ada9440f224827bf7e67e6ba183defb75
Austinjnr <austinobonyo@gmail.com>
2023-03-25 11:22:18 +0300

    cleared files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Register from './components/form/Register';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/navbar/Home';
-import Task from './components/navbar/Task';
 
 function App() {
   return (
-    <div>
-      <Register />
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={Home} exact>
-            <Home />
-          </Route>
-          <Route path="/Task" component={Task} exact>
-            <Task />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <div> </div>
   );
 }
 
